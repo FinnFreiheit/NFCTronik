0da36d0b7505d2b2318955e8b88f685c5b8bde35
Armin Vosoghi Marand <Armin Vosoghi Marand>
2020-08-08 08:46:06 +0200

    Initial Commit

---
+++ LoginFenster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginFenster
+//
+//  Created by Armin Vosoghi Marand on 08.08.20.
+//  Copyright © 2020 NFCtronik-Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
